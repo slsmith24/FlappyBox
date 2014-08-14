bd7d9313b1f1ef4bedfa49fd1cdecb7998f89c3c
slsmith24@cps.edu <>
2014-08-14 17:45:37 +0000

    sham2

--- js/main.js
+++ js/main.js
@@ -42,6 +42,14 @@ var mainState= {
   var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   
   spaceKey.onDown.add(this.jump, this);
+
+  this.pipes = game.add.group();
+  
+  this.pipes.enableBody = true;
+  
+  this.pipes.createMultiple(20,'pipe');
+  
+  this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 },
 
  update: function () {
@@ -56,8 +64,32 @@ var mainState= {
    
  },
  
+ addOnePipe: function (x,y) {
+   // Get the first dead pipe in our group 
+   var pipe = this.pipes.getFirstDead();
+   
+   //Set the new position
+   pipe.reset(x,y);
+ 
+ pipe.body.velocity.x = -200;
+ 
+ pipe.checkWorldBounds = true;
+ pipe.outOfBoundsKill = true; 
+ },
+ 
+ addRowOfPipes: function() {
+   var hole = Math.floor(Math.random() * 5) + 1;
+   
+   for(var i = 0; i < 8; i++)
+     if (i != hole && i != hole +1){
+       
+       this.addOnePipe(400, i*60 + 10);
+     
+     }
+ },
  
  jump: function()  {
+  
    //Let's make our bird jump
     this.bird.body.velocity.y = -350;
 
